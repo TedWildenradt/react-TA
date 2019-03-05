8f1baf85adbd22ede82a9a837b42f23733d7041b
TedWildenradt <tedkwildenradt@gmail.com>
2019-03-04 22:24:49 -0800

    Create entry file

--- form.jsx
+++ form.jsx
@@ -1,8 +1,25 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 
 
 class FormInput extends React.Component{
+  constructor(){
+    this.state = {
+      word = ''
+    }
+  }
 
+
+  render(){
+    return(
+      <div>
+        <form className="search-form">
+          <input type="text" className="search" placeholder="City or State"/>
+
+        </form>
+      </div>
+    )
+  }
 }
 
 document.addEventListener('DOMContentLoaded',()=>{
